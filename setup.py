cf13cdd9557623ff58dfc9a7661bbea751ca22cf
Andrey Cizov <acizov@gmail.com>
2018-03-03 02:51:19 +0000

    Fixes

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ import protobuf_gen as pkg
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read()
 reqs = [x.strip() for x in open('requirements.txt').readlines()]
+test_reqs = [x.strip() for x in open('requirements-tests.txt').readlines()]
 
 setup(
     name=pkg.__name__,
     version=pkg.__version__,
     author=pkg.__author__,
     author_email=pkg.__email__,
-    packages=find_packages(include=(pkg.__name__,)),
+    packages=find_packages(include=(pkg.__name__, pkg.__name__ + '.*',)),
     description='Python 3 type hinted protobuf binding generator',
     keywords='',
     url='https://github.com/andreycizov/python-protobuf-gen',
@@ -19,9 +20,7 @@ setup(
     long_description=readme,
     install_requires=reqs,
     test_suite='tests',
-    tests_require=[
-
-    ],
+    tests_require=test_reqs,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
